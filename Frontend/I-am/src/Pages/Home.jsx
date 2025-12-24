73dd12543a2f9a3ca9419243295279169c9dbc63
SilentStorm227 <stormpetal227@gmail.com>
2025-12-24 13:35:37 +0000

    Home 2

--- Frontend/I-am/src/Pages/Home.jsx
+++ Frontend/I-am/src/Pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "../Style/home.css"
 
 function Home(){
     return(
         <div>
+            <div className="navbar">
             <h1>I am ...</h1>
+            </div>
         </div>
     )
 }
