8e4faa8eaa96f048843d65d876dd5015ebd8dfb6
SilentStorm227 <stormpetal227@gmail.com>
2025-12-24 14:09:35 +0000

    Home 3

--- Frontend/I-am/src/Pages/Home.jsx
+++ Frontend/I-am/src/Pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "../Style/home.css"
+import p1 from "../assets/1.png"
 
 function Home(){
     return(
         <div>
             <div className="navbar">
             <h1>I am ...</h1>
+            <img className="p1" src={p1}></img>
             </div>
-        </div>
+            <h2 className="products">Products</h2>
+    </div>
     )
 }
 
