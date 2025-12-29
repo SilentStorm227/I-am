5c53efd803ccf88bba619ad0945678632b8ad609
SilentStorm227 <stormpetal227@gmail.com>
2025-12-29 17:11:55 +0000

    chains 1 + and - button and cart button ui donee

--- Frontend/I-am/src/Pages/Chains1.jsx
+++ Frontend/I-am/src/Pages/Chains1.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import p1 from "../assets/2.jpg"
 import Slideshow from "./Slideshow";
+import "../Style/chains.css"
 
 function Chains1() {
     return(
         <div>
-            <h1>Flower trouser/skirt chain</h1>
+            <h1 className="text">Flower trouser/skirt chain</h1>
             <Slideshow />
-            <button className="+">+</button>
+            <button className="inc">+</button>
             <input className="num"></input>
-            <button className="-">-</button>
+            <button className="dec">-</button>
+            <br />
+            <button className="cart">Add to cart</button>
         </div>
     )
 }
