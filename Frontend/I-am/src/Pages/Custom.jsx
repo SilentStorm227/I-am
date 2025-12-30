77bd3c0712f364b3da56394b0fe24b92103be714
SilentStorm227 <stormpetal227@gmail.com>
2025-12-30 23:22:52 +0000

    custom order

--- Frontend/I-am/src/Pages/Custom.jsx
+++ Frontend/I-am/src/Pages/Custom.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import "../Style/custom order.css"
 
 function Custom() {
+    const [message, setMessage] = useState("");
+    const [image, setImage] = useState(null);
+    const [success, setSuccess] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append("message", message);
+        if (image) formData.append("image", image);
+
+        const res = await fetch("http://localhost:5000/api/custom-order", {
+            method: "POST",
+            body: formData
+        });
+    }
     return(
         <div>
             <h1 className="text2">Make a custom order today!!</h1>
