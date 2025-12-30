be1762180703d3c674aaa35cf2bc561374c8be6c
SilentStorm227 <stormpetal227@gmail.com>
2025-12-30 14:41:56 +0000

    add to cart real 3

--- Frontend/I-am/src/Pages/Context.jsx
+++ Frontend/I-am/src/Pages/Context.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const  cartContext = createContext();
 
-export function cartProvider({children}){
+export function CartProvider({children}){
     const [cart, setCart] = useState([]);
 
     const addtoCart = (product, quantity) =>{
--- Frontend/I-am/src/main.jsx
+++ Frontend/I-am/src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { cartProvider } from './Pages/Context.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { CartProvider } from "./Pages/Context.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <cartProvider>
-    <App />
-  </cartProvider>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </React.StrictMode>
+);
