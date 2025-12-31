e1d860008f8f7ec00aa1a259525218cad2caad80
SilentStorm227 <stormpetal227@gmail.com>
2025-12-31 13:14:32 +0000

    custom order 2

--- Backend/Server.js
+++ Backend/Server.js
@@ -5,11 +5,17 @@ import customOrderRoutes from "./Routes/CustomOrder";
 
 const web = express();
 
-web.use(cors());
+web.use(cors({
+    origin: "mongodb://localhost:27017/",
+    methods: ["GET", "POST"],
+}));
+
 web.use(express.json());
 web.use("/uploads", express.static("uploads"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/customOrders");
+mongoose.connect("mongodb://127.0.0.1:27017/Iam")
+    .then(()=> console.log("MongoDB connected"))
+    .catch(err => console.err(err));
 
 web.use("/api/custom-order", customOrderRoutes);
 
