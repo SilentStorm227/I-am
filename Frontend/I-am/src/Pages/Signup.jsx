287ffdfac21dd17f0dcff5cd70d9bade83e07e72
SilentStorm227 <stormpetal227@gmail.com>
2026-02-28 14:27:37 +0000

    sign up 2

---
+++ Frontend/I-am/src/Pages/Signup.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+function Signup() {
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handlesignup = async()=>{
+        const res = await fetch("http://localhost:5000/api/users/signup", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                email,
+                name,
+                password
+            })
+    });
+
+    const data = await res.json();
+
+    if(data.success){
+        alert("Account created!!");
+    } else{
+        alert(data.error);
+    }
+};
+
+    return(
+        <div>
+            <h1>Sign up</h1>
+            <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value) } ></input><br /><br />
+
+            <input placeholder="Name" value={name} onChange={(e) => setName(e.target.value) } ></input><br /><br/>
+
+            <input placeholder="Set Passowrd" value={password} onChange={(e) => setPassword(e.target.value)} ></input><br /><br />
+
+            <button onClick={handlesignup}>Sign up</button>
+        </div>
+    )
+}
+
+export default Signup;
