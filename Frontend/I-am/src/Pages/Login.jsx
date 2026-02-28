36c08ba7a9c55b64cdb8c077928ccf7dbe8e7742
SilentStorm227 <stormpetal227@gmail.com>
2026-02-28 16:19:21 +0000

    Login 1

--- Frontend/I-am/src/Pages/Login.jsx
+++ Frontend/I-am/src/Pages/Login.jsx
@@ -6,7 +6,11 @@ function Login() {
             <h1>Login</h1>
 
             <input placeholder="Name"></input><br /><br />
+
             <input placeholder="Password" type="password"></input><br /><br />
+
+
+            <button>Login</button>
         </div>
     )
 }
