dab883f88666156394db5d8fbaf6413a6ddd0b43
SilentStorm227 <stormpetal227@gmail.com>
2026-03-30 12:27:58 +0100

    review

--- Backend/Routes/review.js
+++ Backend/Routes/review.js
@@ -26,8 +26,13 @@ router.get("/api/reviews/:productId", async (req, res) => {
 // POST a review
 router.post("/api/reviews", upload.single("image"),  async (req, res) => {
     try {
-        const newReview = new Review(req.body);
-        await newReview.save();
+        const newReview = new Review({
+            productId: req.body.productId,
+            message: req.body.message,
+            rating: req.body.rating,
+            image: req.file ? req.file.filename : null
+        });
+                await newReview.save();
         res.json(newReview);
     } catch (err) {
         res.status(500).json({ message: "Error saving review" });
