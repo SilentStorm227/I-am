e34bd23e560a57d2f5d9ac61f35406a0bbd1311f
SilentStorm227 <stormpetal227@gmail.com>
2026-03-30 14:24:27 +0100

    css

--- Frontend/I-am/src/Pages/Chains1.jsx
+++ Frontend/I-am/src/Pages/Chains1.jsx
@@ -64,73 +64,6 @@ const average =
     reviews.reduce((sum, r) => sum + r.rating, 0) / reviews.length || 0;
 
     return(
-        // <div>
-        //     <h1 className="text">Flower trouser/skirt chain</h1>
-        //     <h2>Overall rating: {average.toFixed(1)} / 5 ⭐</h2>
-
-        //     <Slideshow />
-
-        //     <button className="inc" onClick={inc} >+</button>
-
-        //     <input className="num" type="number" min="1" step="1" value={quantity}
-        //     onChange={(e) =>{
-        //         let value = Number(e.target.value);
-
-        //         if (value < 1) value = 1;
-        //         if (value > stock) value = stock;
-
-        //         setQuantity(value);
-        //     }}></input>
-
-        //     <button className="dec" onClick={dec} disabled={quantity === 1} >-</button>
-
-        //     <br />
-
-        //     <button className="cart" onClick={()=>
-        //         addtoCart(
-        //             {
-        //                 id:1,
-        //                 name:"flower chain",
-        //                 price:20,
-        //             },
-        //             quantity
-        //         )
-        //     }>Add to cart</button>
-
-        //     <h2>Leave a review</h2>
-
-        //     <form onSubmit={handleReviewSubmit}>
-        //         <textarea placeholder="Write your review.." value={reviewMessage} onChange={(e) => setReviewMessage(e.target.value)} required />
-
-        //             <br />
-
-        //         <input type="number" min="1" max="5" value={rating} onChange={(e) => setRating(e.target.value)} />
-
-        //         <p>Please rate from 1 to 5</p>
-
-        //         <input type="file" accept="image/*" onChange={(e) => setReviewImage(e.target.files[0])} />
-
-        //         <br />
-
-        //         <button type="submit">Send reviews</button>
-
-        //     </form>
-
-        //     <h2>Customer reviews</h2>
-
-        //     {reviews.map((r) => (
-        //         <div key={r._id} style={{border:"1px solid #ccc", padding:10, marginBottom:10}}>
-        //             <p>⭐ {r.rating}/5</p>
-        //             <p>{r.message}</p>
-
-        //             {r.image && (
-        //                 <img src={`http://localhost:5000/uploads/${r.image}`} width="150" />
-        //             )}
-        //         </div>
-        //     ))}            
-
-        // </div>   
-
         <div className="chains-container">
     <h1 className="text">Flower Trouser/Skirt Chain</h1>
     <h2>Overall rating: {average.toFixed(1)} / 5 ⭐</h2>
@@ -146,7 +79,7 @@ const average =
     </div>
 
     <button className="cart" onClick={()=>
-        addtoCart({id:1, name:"flower chain", price:20}, quantity)
+        addtoCart({id:1, name:"flower chain", price:20, quantity: quantity} )
     }>Add to cart</button>
 
     <h2>Leave a Review</h2>
