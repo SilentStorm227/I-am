8cf8c479a6ebc6ed9d5276edf9b127fae34d98c0
SilentStorm227 <stormpetal227@gmail.com>
2026-03-30 16:42:48 +0100

    stripe

--- Backend/Server.js
+++ Backend/Server.js
@@ -5,6 +5,9 @@ import customOrderRoutes from "./Routes/CustomOrder.js";
 import dotenv from "dotenv";
 import userRoutes from './Routes/User.js'
 import reviewRoutes from "./Routes/review.js";
+import Stripe from "stripe";
+
+const stripe = new Stripe("WeLove@nimals");
 
 dotenv.config();
 const web = express();
@@ -25,6 +28,36 @@ web.use("/api/custom-order", customOrderRoutes);
 web.use("/api/users", userRoutes);
 
 web.use(reviewRoutes);
+
+web.post("/api/checkout", async (req, res) =>{
+    try{
+        const {cart} = req.body;
+        const lineItems = cart.map(item => ({
+            price_data: {
+                currency: "gbp",
+                product_data: {
+                    name: item.name,
+                },
+                unit_amount: item.price * 100
+            },
+            quantity: item.quantity,
+        }));
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+                        line_items: lineItems,
+            mode: "payment",
+            success_url: "http://localhost:5173/success",
+            cancel_url: "http://localhost:5173/cart",
+        });
+
+        res.json({url: session.url});
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({message: "stripe error" });
+    }
+});
 
 web.listen(5000, () => {
     console.log("Server running on http://localhost:5000");
